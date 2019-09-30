245dff5fc5bc941f7290754122214777212bd454
tomni <tom.nicholson1000@gmail.com>
2019-09-30 23:47:00 +0700

    Updates

---
+++ precache-manifest.82eb0d8cc93159a89759f89c8a77ed5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3eec5a53d07daf0d8eec",
+    "url": "/Pollution-App/static/js/runtime~main.3eec5a53.js"
+  },
+  {
+    "revision": "f05aad67692bef8df049",
+    "url": "/Pollution-App/static/js/main.f05aad67.chunk.js"
+  },
+  {
+    "revision": "e432d4aa18a5e80a27f2",
+    "url": "/Pollution-App/static/js/1.e432d4aa.chunk.js"
+  },
+  {
+    "revision": "f05aad67692bef8df049",
+    "url": "/Pollution-App/static/css/main.a2594cf6.chunk.css"
+  },
+  {
+    "revision": "43e52cfb55a6b20af8b8a4a734c1ae26",
+    "url": "/Pollution-App/index.html"
+  }
+];
